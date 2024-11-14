1a35b5b041a2f46f7299f3bc60c7dbd531ca00f2
wastech <fataiwasiu2@gmail.com>
2024-11-14 22:47:31 +0100

    Setup :art:

---
+++ src/test/java/com/example/blogify/BlogifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
